efc5a2f9e50312a924822c5e27a74505d701446f
833FCAK3 <stiwl@mail.ru>
2025-03-25 15:27:51 +0500

    separate data fetching logic in index and view

--- phone_api/views.py
+++ phone_api/views.py
@@ -1,27 +1,45 @@
+import json
+
 import requests
-from django.http import JsonResponse
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 
 
 # Create your views here.
-def index(request):
+def index(request) -> HttpResponse:
     return render(request, "index.html")
 
 
-def search_phone(request):
-    phone_number = request.GET.get("num")
-    if not phone_number:
-        return JsonResponse({"error": "No phone number provided"}, status=400)
+def search_phone(request) -> JsonResponse:
+    print(type(request))
+    if request.method == "GET":
+        phone_number = request.GET.get("num")
+        if not phone_number:
+            return JsonResponse({"error": "No phone number provided"}, status=400)
+        phone_number = edit_number(phone_number)
 
-    url = f"https://opendata.digital.gov.ru/api/v1/abcdef/phone?num={phone_number}"
+        url = f"https://opendata.digital.gov.ru/api/v1/abcdef/phone?num={phone_number}"
 
-    headers = {
-        "User-Agent": "Mozilla/5.0",
-    }
+        headers = {
+            "User-Agent": "Mozilla/5.0",
+        }
 
-    response = requests.get(url, headers=headers)
+        try:
+            response = requests.get(url, headers=headers)
+            response.raise_for_status()  # Raise an error for bad status codes
+            data = {
+                "phone_number": phone_number,
+                "operator": json.loads(response.content)["data"][0]["operator"],
+                "region": json.loads(response.content)["data"][0]["region_gar"],
+            }
+            return JsonResponse(data, json_dumps_params={"ensure_ascii": False})
+        except requests.exceptions.RequestException as e:
+            return JsonResponse({"error": f"Failed to fetch data: {str(e)}"}, status=500)
 
-    if response.status_code == 200:
-        return JsonResponse(response.json())
-    else:
-        return JsonResponse({"error": f"Failed to fetch data ({response.status_code})"}, status=response.status_code)
+    return JsonResponse({"error": "Unsupported request method"}, status=405)
+
+
+def edit_number(number: str) -> str:
+    if len(number) == 10:
+        number = "7" + number
+    return number
