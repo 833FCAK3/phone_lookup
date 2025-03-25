07a7721e8cca22896c6b765c12b90e09d4655ba5
833FCAK3 <stiwl@mail.ru>
2025-03-25 17:16:17 +0500

    fix caching

--- phone_api/views.py
+++ phone_api/views.py
@@ -13,9 +13,7 @@ def index(request) -> HttpResponse:
     return render(request, "index.html")
 
 
-@lru_cache
 def search_phone(request) -> JsonResponse:
-    print(type(request))
     if request.method == "GET":
         phone_number = request.GET.get("num")
         if not phone_number:
@@ -24,34 +22,40 @@ def search_phone(request) -> JsonResponse:
 
         url = f"https://opendata.digital.gov.ru/api/v1/abcdef/phone?num={phone_number}"
 
-        headers = {
-            "User-Agent": "Mozilla/5.0",
-        }
-
         try:
-            response = requests.get(url, headers=headers)
-            response.raise_for_status()  # Raise an error for bad status codes
-            operator = json.loads(response.content)["data"][0]["operator"]
-            region = json.loads(response.content)["data"][0]["region_gar"]
-
-            data = {
-                "phone_number": phone_number,
-                "operator": operator,
-                "region": region,
-            }
-
-            new_phone = PhonesLookedUp.objects.create(
-                phone_number=phone_number,
-                operator=operator,
-                region=region,
-            )
-            new_phone.save()
+            data = lookup_phone(url, phone_number)
 
             return JsonResponse(data, json_dumps_params={"ensure_ascii": False})
         except requests.exceptions.RequestException as e:
             return JsonResponse({"error": f"Failed to fetch data: {str(e)}"}, status=500)
 
     return JsonResponse({"error": "Unsupported request method"}, status=405)
+
+
+@lru_cache
+def lookup_phone(url: str, phone_number: str):
+    headers = {
+        "User-Agent": "Mozilla/5.0",
+    }
+    response = requests.get(url, headers=headers)
+    response.raise_for_status()  # Raise an error for bad status codes
+    operator = json.loads(response.content)["data"][0]["operator"]
+    region = json.loads(response.content)["data"][0]["region_gar"]
+
+    data = {
+        "phone_number": phone_number,
+        "operator": operator,
+        "region": region,
+    }
+
+    new_phone = PhonesLookedUp.objects.create(
+        phone_number=phone_number,
+        operator=operator,
+        region=region,
+    )
+    new_phone.save()
+
+    return data
 
 
 def edit_number(number: str) -> str:
